2ff52eb67b6877e2775a787dc88e058697b628be
JoaoPauloF2005 <hypergamerfrr@gmail.com>
2022-06-07 21:35:05 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadeDs/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace AtividadeDs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProduto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQtde = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.ListProduto = new System.Windows.Forms.ListView();
+            this.Coluna1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Coluna2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Coluna3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Produto:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtProduto
+            // 
+            this.txtProduto.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProduto.Location = new System.Drawing.Point(193, 24);
+            this.txtProduto.Name = "txtProduto";
+            this.txtProduto.Size = new System.Drawing.Size(425, 25);
+            this.txtProduto.TabIndex = 1;
+            this.txtProduto.TextChanged += new System.EventHandler(this.txtProduto_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(82, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Quantidade:";
+            // 
+            // txtQtde
+            // 
+            this.txtQtde.Location = new System.Drawing.Point(193, 71);
+            this.txtQtde.Name = "txtQtde";
+            this.txtQtde.Size = new System.Drawing.Size(284, 20);
+            this.txtQtde.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(78, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Vlr. Unitario:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(193, 129);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(284, 20);
+            this.txtValor.TabIndex = 5;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.Location = new System.Drawing.Point(336, 170);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(115, 61);
+            this.btnInserir.TabIndex = 6;
+            this.btnInserir.Text = "Inserir Produto";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(190, 600);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total da Venda:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.Blue;
+            this.lblTotal.Location = new System.Drawing.Point(367, 600);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(41, 16);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "label5";
+            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(193, 655);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(125, 64);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Nova Venda";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(445, 655);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(125, 64);
+            this.btnFechar.TabIndex = 11;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // ListProduto
+            // 
+            this.ListProduto.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Coluna1,
+            this.Coluna2,
+            this.Coluna3});
+            this.ListProduto.HideSelection = false;
+            this.ListProduto.Location = new System.Drawing.Point(110, 246);
+            this.ListProduto.Name = "ListProduto";
+            this.ListProduto.Size = new System.Drawing.Size(510, 326);
+            this.ListProduto.TabIndex = 12;
+            this.ListProduto.UseCompatibleStateImageBehavior = false;
+            this.ListProduto.View = System.Windows.Forms.View.Details;
+            // 
+            // Coluna1
+            // 
+            this.Coluna1.Text = "Produto";
+            this.Coluna1.Width = 140;
+            // 
+            // Coluna2
+            // 
+            this.Coluna2.Text = "Quantidade";
+            this.Coluna2.Width = 161;
+            // 
+            // Coluna3
+            // 
+            this.Coluna3.Text = "Valor Unitário";
+            this.Coluna3.Width = 154;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 744);
+            this.Controls.Add(this.ListProduto);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtQtde);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProduto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProduto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQtde;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.ListView ListProduto;
+        private System.Windows.Forms.ColumnHeader Coluna1;
+        private System.Windows.Forms.ColumnHeader Coluna2;
+        private System.Windows.Forms.ColumnHeader Coluna3;
+    }
+}
+
---
+++ AtividadeDs/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadeDs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double resultado;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtProduto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            string produto = txtProduto.Text;
+            int quantidade = int.Parse(txtQtde.Text);
+            double valor_untario = double.Parse(txtValor.Text);
+
+            resultado = quantidade * valor_untario;
+
+            lblTotal.Text = resultado.ToString();
+
+            ListViewItem item = new ListViewItem(txtProduto.Text);
+            item.SubItems.Add(txtQtde.Text);
+            item.SubItems.Add(txtValor.Text);
+            ListProduto.Items.Add(item);
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtProduto.Clear();
+            txtQtde.Clear();
+            txtValor.Clear();
+            lblTotal.Text = "0";
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lblTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
